2474aefdd08a61d6ea0e6ad1de4e8a16f54d9c9a
anjarupnik <anjarupnik@hotmail.com>
2018-01-29 19:41:52 +0100

    queries

--- src/schema/movie.js
+++ src/schema/movie.js
@@ -1,7 +1,47 @@
-export const Movie = `
-  type Movie {
+import { makeExecutableSchema } from 'graphql-tools';
+
+export const typeDefs = `
+  interface Media {
     id: ID!
     title: String!
-    budget(currency: Currency = EUR): Int
+    media_type: String!
+  }
+
+  type Movie implements Media {
+    id: ID!
+    title: String!
+    media_type: String!
+    duration: Int!
+    box_office: Int!
+  }
+
+  type TVShow implements Media {
+    id: ID!
+    title: String!
+    media_type: String!
+    running: Boolean
+  }
+
+  type Person {
+    name: String!
+    known_for: [Media]
+  }
+
+  type Query {
+    movies: [Movie]
+    movie(id: ID, imdb_id: String): Movie
+  }
+
+  type Mutation {
+    rateMovie (
+      title: String!
+      RatingInput: Int!
+    ): Movie
   }
 `;
+
+const schema = makeExecutableSchema({
+  typeDefs
+});
+
+export default schema;
